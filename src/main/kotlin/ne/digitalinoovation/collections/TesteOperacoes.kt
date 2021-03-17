e7342c2c5e714618b25c1aa09ef44fb98de45df8
lvinicio <lucas.vinicius.oliveira@everis.com>
2021-03-17 16:00:02 -0300

    Operações

---
+++ src/main/kotlin/ne/digitalinoovation/collections/TesteOperacoes.kt
@@ -0,0 +1,33 @@
+package ne.digitalinoovation.collections
+
+fun main(){
+
+    val salarios = doubleArrayOf(1000.0, 2250.0, 4000.0)
+
+    for (salario in salarios ){
+        println(salario)
+    }
+    println("--------------------------")
+    println("Maior salário ${salarios.maxOrNull()}")
+    println("Menor salário ${salarios.minOrNull()}")
+    println("Média salário ${salarios.average()}")
+
+val salariosMaiorQue2500 = salarios.filter { it > 2500.0 }
+    println("--------------------------")
+    salariosMaiorQue2500.forEach{println(it)}
+
+    println("--------------------------")
+
+    println(salarios.count { it in 2000.0..5000.0})
+
+    println("--------------------------")
+    println(salarios.find { it == 2250.0 })
+    println(salarios.find { it == 500.0 })
+
+    println("--------------------------")
+    println(salarios.any { it == 2250.0 })
+    println(salarios.any { it == 500.0 })
+
+
+
+}
